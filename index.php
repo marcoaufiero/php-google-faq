32cdce6079fd0485bc97f16c10e2dd1040b96e31
marcoaufiero <marcoaufiero96@gmail.com>
2022-12-14 12:49:35 +0100

    header top added

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://kit.fontawesome.com/20d85c6a60.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    
+    <header>
+        <div class="header-top">
+            <div class="header-top-left">
+                <img src="./assets/img/logo_google.png" alt="">
+                <span>Privacy e termini</span>
+            </div>
+            <div class="header-top-right">
+                <i class="fa-solid fa-table-cells"></i>
+                <div class="profile-box">
+                    <span>M</span>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+</body>
+</html>
